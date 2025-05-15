a06fbba5918e8608f9fd67ac8a65d71e94d645aa
ahmethusrevkaratas <ahmethusrevkaratas@gmail.com>
2025-05-15 16:59:52 +0300

    Update video_maker.py

--- utils/video_maker.py
+++ utils/video_maker.py
@@ -1,1 +1,9 @@
+from moviepy.editor import ImageClip, AudioFileClip, concatenate_videoclips
+
+def make_video(audio_path, image_path, output="output_video.mp4"):
+    audio = AudioFileClip(audio_path)
+    image = ImageClip(image_path).set_duration(audio.duration)
+    video = image.set_audio(audio)
+    video.write_videofile(output, fps=24)
+    return output
 
