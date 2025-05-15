4bee306737732c467dc457a7739d2c3a28a5c6af
ahmethusrevkaratas <ahmethusrevkaratas@gmail.com>
2025-05-15 16:59:03 +0300

    Update text_gen.py

--- utils/text_gen.py
+++ utils/text_gen.py
@@ -1,1 +1,11 @@
+def generate_story(topic="gizli tarih", length=500):
+    # Basit örnek: Sabit metin döndürüyor
+    story = (
+        f"Bu bir {topic} ile ilgili kurgusal hikayedir.\n"
+        "Yüzyıllar önce gizli kalmış sırlar ortaya çıkmaya başladı.\n"
+        "Bir grup araştırmacı bu gizemi çözmek için tehlikeli bir yolculuğa çıktı.\n"
+        "Her adımda daha fazla sır açığa çıktı ve gerçekler değişmeye başladı...\n"
+        "Bu hikaye burada başlıyor..."
+    )
+    return story
 
