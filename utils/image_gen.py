bc8fbe23f2891c93087b428e82d004b173865877
ahmethusrevkaratas <ahmethusrevkaratas@gmail.com>
2025-05-15 16:59:34 +0300

    Update image_gen.py

--- utils/image_gen.py
+++ utils/image_gen.py
@@ -1,1 +1,10 @@
+from PIL import Image, ImageDraw, ImageFont
+
+def generate_image(text="Gizli Tarih Teması", filename="output.png"):
+    # 640x480 beyaz arka planlı basit görsel oluşturuyor
+    img = Image.new('RGB', (640, 480), color='white')
+    d = ImageDraw.Draw(img)
+    d.text((10, 200), text, fill=(0, 0, 0))
+    img.save(filename)
+    return filename
 
